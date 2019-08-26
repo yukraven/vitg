06408662645afe3f639a59e7e318fd9ffdb0ee0b
lavrik.d <lavrik.d@artix.su>
2019-08-26 17:48:43 +0700

    Weather and startDescription

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ LocJs = json.load(open("Locations.json"))
 
 for i in LocJs:
     temp = LocJs[i]
-    location = R.Location(temp["description"], temp["expectedCommands"],
+    location = R.Location(temp["startDescription"], temp["description"], temp["expectedCommands"],
                           temp["weather"])
     print(repr(location))
     print(location)
-    print(location())
+    print(location.getHi())
 
 
