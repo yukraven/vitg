217c7aa15732a547652490ed94871ae1c2fbb10e
lavrik.d <lavrik.d@artix.su>
2019-08-30 11:23:34 +0700

    Properties of weather... are private

--- Resources/Location.py
+++ Resources/Location.py
@@ -4,13 +4,13 @@ from Resources.Resource import Resource
 
 class Location(Resource):
     # Current weather, can change every turn
-    weather = {"downfall": 0,               # 0 - clear, 1 - rain, 2 - hail, 3 - snow
-               "wind": 0,                   # from 0 to 100 (1 points = 1 km/s)
-               "wet": 0,                    # from 0 to 100 (%)
-               "temperature": 0,            # from 0 to 100 (1 point = 2 degrees Celsius, 50 = 0)
-               "thunder": False}            # does thunder sound in the current turn
+    _weather = {"downfall": 0,               # 0 - clear, 1 - rain, 2 - hail, 3 - snow
+                "wind": 0,                   # from 0 to 100 (1 points = 1 km/s)
+                "wet": 0,                    # from 0 to 100 (%)
+                "temperature": 0,            # from 0 to 100 (1 point = 2 degrees Celsius, 50 = 0)
+                "thunder": False}            # does thunder sound in the current turn
     # Arrays of probabilities
-    weatherChances = {"downfall": {"0": [100, 0, 0, 0],                 # transitions 0->0, 0->1, 0->2, 0->3
+    _weatherChances = {"downfall": {"0": [100, 0, 0, 0],                 # transitions 0->0, 0->1, 0->2, 0->3
                                    "1": [0, 100, 0, 0],                 # transitions 1->0, 1->1, 1->2, 1->3
                                    "2": [0, 0, 100, 0],                 # transitions 2->0, 2->1, 2->2, 2->3
                                    "3": [0, 0, 0, 100]},                # transitions 3->0, 3->1, 3->2, 3->3
@@ -26,7 +26,7 @@ class Location(Resource):
                                   "1": {"wind": 30, "wet": 0, "temperature": 0, "probability": 50},
                                   "2": {"wind": 0, "wet": 0, "temperature": 0, "probability": 0},
                                   "3": {"wind": 0, "wet": 0, "temperature": 0, "probability": 0}}}
-    weatherChanging = {"downfall": {"0": ["", "", "", ""],
+    _weatherChanging = {"downfall": {"0": ["", "", "", ""],
                                     "1": ["", "", "", ""],
                                     "2": ["", "", "", ""],
                                     "3": ["", "", "", ""]},
@@ -44,21 +44,21 @@ class Location(Resource):
     def __init__(self, startDescription, description, expectedCommands, weather, weatherChances, weatherChanging):
         """ Initialization """
         Resource.__init__(self, startDescription, description, expectedCommands)
-        self.weather = weather
-        self.weatherChances = weatherChances
-        self.weatherChanging = weatherChanging
+        self._weather = weather
+        self._weatherChances = weatherChances
+        self._weatherChanging = weatherChanging
 
     def __repr__(self):
         """ Complements the information by adding about weather and weather chances """
         result = Resource.__repr__(self)
         result += " | "
-        for i in self.weather:
+        for i in self._weather:
             result += " "
-            result += str(self.weather[i])
+            result += str(self._weather[i])
         result += " | "
-        for i in self.weatherChances:
+        for i in self._weatherChances:
             result += " "
-            result += str(self.weatherChances[i])
+            result += str(self._weatherChances[i])
         return result
 
     def __call__(self):
@@ -73,27 +73,27 @@ class Location(Resource):
 
     def _changeDownfall(self):
         """ Changes downfall, returns string information, if changed, else - "" """
-        chances = self.weatherChances["downfall"]
+        chances = self._weatherChances["downfall"]
         for state in chances:
-            if self.weather["downfall"] == int(state):
+            if self._weather["downfall"] == int(state):
                 newDownfall = self._tryChangeDownfall[getRandFromArray(chances[state])](self)
-                if self.weather["downfall"] == newDownfall:             # if nothing changed
+                if self._weather["downfall"] == newDownfall:             # if nothing changed
                     return ""
-                oldDownfall = self.weather["downfall"]
-                self.weather["downfall"] = newDownfall
-                return self.weatherChanging["downfall"][str(oldDownfall)][newDownfall]
+                oldDownfall = self._weather["downfall"]
+                self._weather["downfall"] = newDownfall
+                return self._weatherChanging["downfall"][str(oldDownfall)][newDownfall]
 
     def _changeAtmosphere(self, element, index):
         """ Changes wind, wet or temperature, if changed returns string information, else - "" """
-        chances = self.weatherChances[element]
-        curState = self.weather[element]
+        chances = self._weatherChances[element]
+        curState = self._weather[element]
         for state in chances:
             if curState >= int(state):
                 if index == "random":
                     indexOfMod = getRandFromArray(chances[state])
                 else:
                     indexOfMod = index
-                result = self.weatherChanging[element][str(indexOfMod)]
+                result = self._weatherChanging[element][str(indexOfMod)]
                 curState += self.getModByIndex[indexOfMod]
                 if curState < 0:
                     curState = 0
@@ -101,25 +101,25 @@ class Location(Resource):
                 elif curState > 100:
                     curState = 100
                     result = ""
-                self.weather[element] = curState
+                self._weather[element] = curState
                 return result
 
     def _getThunder(self):
         """ Adds sound of thunder or not """
-        chances = self.weatherChances["thunder"][str(self.weather["downfall"])]
+        chances = self._weatherChances["thunder"][str(self._weather["downfall"])]
         prob = chances["probability"]
         if prob > 0:
             for i in ["wind", "wet", "temperature"]:
-                if self.weather[i] < chances[i]:
+                if self._weather[i] < chances[i]:
                     return ""
             if getRandFromArray([prob, 100-prob]) == 0:
-                return self.weatherChanging["thunder"]
+                return self._weatherChanging["thunder"]
         return ""
 
     def _tryRain(self):
         """ Try change downfall for rain, changes wet, return new state """
-        temperatureFor = self.weatherChances["tempAndDownfall"]
-        temperature = self.weather["temperature"]
+        temperatureFor = self._weatherChances["tempAndDownfall"]
+        temperature = self._weather["temperature"]
         if temperature <= temperatureFor["rainFreezes"]:
             newState = self._tryHail()
         elif temperature >= temperatureFor["rainEvaporates"]:
@@ -132,8 +132,8 @@ class Location(Resource):
 
     def _tryHail(self):
         """ Try change downfall for hail, changes temperature, return new state """
-        temperatureFor = self.weatherChances["tempAndDownfall"]
-        temperature = self.weather["temperature"]
+        temperatureFor = self._weatherChances["tempAndDownfall"]
+        temperature = self._weather["temperature"]
         if temperature >= temperatureFor["hailMelts"]:
             newState = self._tryRain()
         else:
@@ -143,8 +143,8 @@ class Location(Resource):
 
     def _trySnow(self):
         """ Try change downfall for snow, changes temperature, return new state """
-        temperatureFor = self.weatherChances["tempAndDownfall"]
-        temperature = self.weather["temperature"]
+        temperatureFor = self._weatherChances["tempAndDownfall"]
+        temperature = self._weather["temperature"]
         if temperature >= temperatureFor["snowMelts"]:
             newState = self._tryRain()
         else:
