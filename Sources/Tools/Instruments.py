158095e3feeedf1d83a17978f8bba3615d5729c4
lavrik.d <lavrik.d@artix.su>
2019-09-10 11:51:27 +0700

    #19 did it

--- Sources/Tools/Instruments.py
+++ Sources/Tools/Instruments.py
@@ -3,27 +3,36 @@ from Sources.Tools.Exceptions import ValueIsNotProbabilityError
 from Sources.Tools.Exceptions import SumOfProbabilitiesIsNot100Error
 
 
-def getRandFromArray(array):
+def getRandFromArray(array, withWhat):
     """ Returns a random index from an array of probabilities """
     if type(array) is not list:
         raise TypeError
     if len(array) == 0:
         raise ValueError
-    for i in array:
-        if i < 0 or i > 100:
-            raise ValueIsNotProbabilityError(i)
-    sumOfProbabilities = 0
-    for i in array:
-        sumOfProbabilities += i
-    if sumOfProbabilities != 100:
-        raise SumOfProbabilitiesIsNot100Error
 
-    temp = 0
-    resultArray = []
-    for i in array:
-        temp += i
-        resultArray.append(temp)             # Converting of the array into a form convenient for calculation
-    randomValue = random.randint(1, 100)
-    for i in range(len(resultArray)):
-        if randomValue <= resultArray[i]:
-            return i
+    if withWhat == "withProbs":
+        for i in array:
+            if not (isinstance(i, int) or isinstance(i, float)):
+                raise ValueError
+        for i in array:
+            if i < 0 or i > 100:
+                raise ValueIsNotProbabilityError(i)
+        sumOfProbabilities = 0
+        for i in array:
+            sumOfProbabilities += i
+        if sumOfProbabilities != 100:
+            raise SumOfProbabilitiesIsNot100Error
+
+        temp = 0
+        resultArray = []
+        for i in array:
+            temp += i
+            resultArray.append(temp)             # Converting of the array into a form convenient for calculation
+        randomValue = random.randint(1, 100)
+        for i in range(len(resultArray)):
+            if randomValue <= resultArray[i]:
+                return i
+
+    if withWhat == "withValues":
+        randomValue = random.randint(0, len(array) - 1)
+        return array[randomValue]
--- Unittests/testInstruments.py
+++ Unittests/testInstruments.py
@@ -5,36 +5,58 @@ import Sources.Tools.Instruments
 
 class TestGetRandFromArray(unittest.TestCase):
     def testSuccess(self):
-        arrays = [[100, 0, 0, 0],
-                  [0, 100, 0, 0],
-                  [0, 0, 100, 0],
-                  [0, 0, 0, 100]]
+        arraysWithProbs = [[100, 0, 0, 0],
+                           [0, 100, 0, 0],
+                           [0, 0, 100, 0],
+                           [0, 0, 0, 100]]
+        arraysWithValues = [["One value. "],
+                            [["One value. "]],
+                            [{"One": "value"}]]
         for i in range(0, 4):
             with self.subTest(i=i):
-                result = Sources.Tools.Instruments.getRandFromArray(arrays[i])
+                result = Sources.Tools.Instruments.getRandFromArray(arraysWithProbs[i], "withProbs")
 
                 assert result == i
+        for i in range(0, 3):
+            with self.subTest(i=i):
+                result = Sources.Tools.Instruments.getRandFromArray(arraysWithValues[i], "withValues")
+
+                assert result == arraysWithValues[i][0]
 
     def testFailNotArray(self):
-        array = "I'm not array!"
+        array = "I'm not an array!"
 
-        self.assertRaises(TypeError, Sources.Tools.Instruments.getRandFromArray, array)
+        self.assertRaises(TypeError, Sources.Tools.Instruments.getRandFromArray, array, "withProbs")
+        self.assertRaises(TypeError, Sources.Tools.Instruments.getRandFromArray, array, "withValues")
 
     def testFailEmptyArray(self):
         array = []
 
-        self.assertRaises(ValueError, Sources.Tools.Instruments.getRandFromArray, array)
+        self.assertRaises(ValueError, Sources.Tools.Instruments.getRandFromArray, array, "withProbs")
+        self.assertRaises(ValueError, Sources.Tools.Instruments.getRandFromArray, array, "withValues")
+
+    def testFailIsNotNumbersForWithProbs(self):
+        arrays = [["", "", ""],
+                  [[], [], []],
+                  [{}, {}, {}]]
+        for i in range(0, 3):
+            with self.subTest(i=i):
+                self.assertRaises(ValueError, Sources.Tools.Instruments.getRandFromArray, arrays, "withProbs")
 
     def testFailIsNotProbabilities(self):
         array1 = [101, 0]
         array2 = [-1, 100]
 
-        self.assertRaises(Sources.Tools.Exceptions.ValueIsNotProbabilityError, Sources.Tools.Instruments.getRandFromArray, array1)
-        self.assertRaises(Sources.Tools.Exceptions.ValueIsNotProbabilityError, Sources.Tools.Instruments.getRandFromArray, array2)
+        self.assertRaises(Sources.Tools.Exceptions.ValueIsNotProbabilityError,
+                          Sources.Tools.Instruments.getRandFromArray, array1, "withProbs")
+        self.assertRaises(Sources.Tools.Exceptions.ValueIsNotProbabilityError,
+                          Sources.Tools.Instruments.getRandFromArray, array2, "withProbs")
 
     def testFailSumIsNot100(self):
         array1 = [70, 40]
         array2 = [70, 20]
 
-        self.assertRaises(Sources.Tools.Exceptions.SumOfProbabilitiesIsNot100Error, Sources.Tools.Instruments.getRandFromArray, array1)
-        self.assertRaises(Sources.Tools.Exceptions.SumOfProbabilitiesIsNot100Error, Sources.Tools.Instruments.getRandFromArray, array2)
+        self.assertRaises(Sources.Tools.Exceptions.SumOfProbabilitiesIsNot100Error,
+                          Sources.Tools.Instruments.getRandFromArray, array1, "withProbs")
+        self.assertRaises(Sources.Tools.Exceptions.SumOfProbabilitiesIsNot100Error,
+                          Sources.Tools.Instruments.getRandFromArray, array2, "withProbs")
