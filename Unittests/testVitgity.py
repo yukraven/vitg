db88e285a3f784d6e7f986822ac97852a8633be1
lavrik.d <lavrik.d@artix.su>
2019-09-30 14:05:41 +0700

    Test for Vitgity.__init__()
    Success, case when argument is not dictionary and case when dictionary has wrong data or has not some of data

---
+++ Unittests/testVitgity.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest import mock
+import Sources.Vitgity
+
+
+class TestInit(unittest.TestCase):
+    def testSuccess(self):
+        dictionary = {"name": "1", "startDescription": "2", "description": "3", "speed": 4, "actions": ["5"]}
+
+        vitgity = Sources.Vitgity.Vitgity(dictionary)
+        assert vitgity.name == "1"
+        assert vitgity.startDescription == "2"
+        assert vitgity.description == "3"
+        assert vitgity.speed == 4
+        assert vitgity.actions == ["5"]
+
+    def testNotDictionary(self):
+        dictionaries = [[1, 2, 3],
+                        1,
+                        "1"]
+        for i in range(len(dictionaries)):
+            with self.subTest(i=i):
+                vitgity = Sources.Vitgity.Vitgity(dictionaries[i])
+                assert vitgity.name == "ErrorLoad"
+                assert vitgity.description == "Bad argument"
+
+    def testWrongData(self):
+        dictionaries = [{"name": 1, "startDescription": "2", "description": "3", "speed": 4, "actions": ["5"]},
+                        {"name": "1", "startDescription": 2, "description": "3", "speed": 4, "actions": ["5"]},
+                        {"name": "1", "startDescription": "2", "description": 3, "speed": 4, "actions": ["5"]},
+                        {"name": "1", "startDescription": "2", "description": "3", "speed": "4", "actions": ["5"]},
+                        {"name": "1", "startDescription": "2", "description": "3", "speed": 4, "actions": 5},
+                        {"name": "1", "startDescription": "2", "description": "3", "speed": 4, "actions": [5]},
+
+                        {"startDescription": "2", "description": "3", "speed": 4, "actions": ["5"]},
+                        {"name": "1", "description": "3", "speed": 4, "actions": ["5"]},
+                        {"name": "1", "startDescription": "2", "speed": 4, "actions": ["5"]},
+                        {"name": "1", "startDescription": "2", "description": "3", "actions": ["5"]},
+                        {"name": "1", "startDescription": "2", "description": "3", "speed": 4}]
+        errors = ["name", "startDescription", "description", "speed", "actions",
+                  "name", "startDescription", "description", "speed", "actions"]
+
+        dictionaryForManyErrors = {"name": "1", "description": "3", "speed": "4"}
+        errorArray = ["startDescription", "speed", "actions"]
+
+        for i in range(len(dictionaries)):
+            with self.subTest(i=i):
+                vitgity = Sources.Vitgity.Vitgity(dictionaries[i])
+                assert vitgity.name == "ErrorLoad"
+                assert vitgity.description[0] == errors[i]
+
+        vitgity = Sources.Vitgity.Vitgity(dictionaryForManyErrors)
+        assert vitgity.name == "ErrorLoad"
+        assert vitgity.description == errorArray
